6d4b26c5aca0a4cb6fb429dc1c5f8c4c2f038dbc
Gim128 <gimhanpabasara4@gmail.com>
2021-05-22 23:23:14 +0530

    full animation

---
+++ Majic.js
@@ -0,0 +1,29 @@
+$(document).ready(function (){
+    $surface = $('.surface');
+    $car = $('.car');
+    $img = $('.car img');
+    let flag = true;
+
+
+    const car = ['./asserts/Img_05.png','./asserts/Img_06.png']
+
+    $(document).on('keypress',function (e){
+        if (e.which == 13){
+            $($surface).toggleClass('moveRight');
+            $($car).toggleClass('suspensions');
+        }
+    })
+
+    $(document).on('keypress',function (e){
+        if (e.which == 119){
+            if (flag){
+                flag = false;
+                $img.attr('src',car[0]);
+            }else {
+                flag = true;
+                $img.attr('src',car[1]);
+            }
+        }
+    })
+
+})
